3d8b22eb3fccb0eee104fa2e94340ec7f6aa2393
Galina Antipin <galina.antipin@gmail.com>
2024-09-19 11:46:28 +0200

    added a new function @output and EventEmitter

--- src/app/all/fruitlist/singlefruit/singlefruit.component.ts
+++ src/app/all/fruitlist/singlefruit/singlefruit.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input , Output} from '@angular/core';
 
 @Component({
   selector: 'app-singlefruit',
@@ -16,4 +16,10 @@ export class SinglefruitComponent {
   stars: 2.3,
   reviews:[{name: "Waldemar W.", text: "gut für Obstsalat"},{name: "Olaf P.", text: "Kann man mal machen"}],
 };
+
+@Output()fruitname = new EventEmitter<string>();
+
+emitName(){
+  this.fruitname.emit(this.fruit.name)
+}
 }
