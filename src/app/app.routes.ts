f48fa705ba8efca49c51588f4bb85eca6a76b6e3
Galina Antipin <galina.antipin@gmail.com>
2024-09-19 10:34:38 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FruitlistComponent } from './all/fruitlist/fruitlist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: FruitlistComponent},
+];
